4ad1d68dda8a71766a4c84e6a39d05d914dd9f5f
Maryana Kachanava <m.kachanava@godeltech.com>
2019-03-15 14:21:26 +0300

    remove useless dir

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,12 @@ import { FormsModule }  from '@angular/forms';
 import { AppComponent } from './app.component';
 import { InputComponentComponent } from './input-component/input-component.component';
 import { PlaceholderDirective } from './placeholder.directive';
-import { ValidationDirective } from './validation.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     InputComponentComponent,
     PlaceholderDirective,
-    ValidationDirective,
   ],
   imports: [
     BrowserModule,
